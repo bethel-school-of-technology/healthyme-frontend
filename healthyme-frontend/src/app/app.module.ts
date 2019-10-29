99d869cefd703f897d83310b99f4d5cfee301789
Jordan Page <pagejor023@gmail.com>
2019-10-29 16:46:54 -0500

    Added components

--- healthyme-frontend/src/app/app.module.ts
+++ healthyme-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BlogComponent,
+    ToolsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
