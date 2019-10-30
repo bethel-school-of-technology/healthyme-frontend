c66e2d242f65221cb96bde1a69276a1568a355e0
Ian Ditsch <44419037+FS-SoftEng@users.noreply.github.com>
2019-10-30 17:40:59 -0500

    Start
    First copy commit

--- healthyme-frontend/src/app/app.module.ts
+++ healthyme-frontend/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BlogComponent,
+    ToolsComponent,
+    HomeComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
