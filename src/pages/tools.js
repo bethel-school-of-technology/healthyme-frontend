7ab81dcf0684ad6f6b5c62a180790dd73eeecc89
Jason Walker <jwalkerus30405@gmail.com>
2019-11-30 11:05:18 -0600

    updates to tool page

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -10,7 +10,7 @@ export default class Tools extends Component {
   render() {
     return (
       <div className="Tools">
-        <div className="">Exercise Tools</div>
+        {/* <div className="">Exercise Tools</div> */}
         <div className="Timers">
           <Stopwatch />
           <Countdown />
